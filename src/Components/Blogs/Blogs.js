901fe546b579a1ffde54b526752c183b024c9e58
shely25 <shely.ece.25@gmail.com>
2022-04-19 03:20:00 +0600

    blogs added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,7 +10,31 @@ const Blogs = () => {
             </div>
             <div>
                 <h3>Why am I using Firebase? What other options do you have to implement authentication?</h3>
-                <p>We are using firebase for many purposes like Authentication,Hosting, Cloud masseging, remote Configuration ect.</p>
+                <p>Google firebase is a google backed application development software that enables developer to develop iOS,android,web applications. We are using firebase for many purposes like Authentication,Hosting, Cloud masseging, remote Configuration ect.Here we can find many advantages of using firebase.The advantages are it has reliable and extensive databases , realtime databases,cloud firestore,fast and safe hosting,free use of firebase dynamic link.easy to use authentication system.</p>
+                <p>other Option:
+                    <ul>
+                        <li>Parse</li>
+                        <li>Back4App</li>
+                        <li>AWS Amplify</li>
+                        <li>Kuzzle</li>
+                        <li>Couchbase</li>
+                        <li>NativeScript</li>
+                    </ul>
+
+                </p>
+            </div>
+            <div>
+                <h3>What other services does firebase provide other than authentication?</h3>
+                <p>Other Services:
+                    <ul>
+                        <li>Cloud Firestore</li>
+                        <li>Cloud Functions</li>
+                        <li>Hosting</li>
+                        <li>Cloud Storage</li>
+                        <li>Google Analitic</li>
+                        <li>Predictions</li>
+                    </ul>
+                </p>
             </div>
         </div>
     );
