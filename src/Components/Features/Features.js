6b3dc03ad2e2cb8f1deda0c538113c8011951a23
shely25 <shely.ece.25@gmail.com>
2022-04-19 13:28:45 +0600

    wabpge completed 2

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -5,9 +5,9 @@ const Features = () => {
         <div className='mt-5 text-center'>
             <h1 style={{ color: 'goldenrod' }}>Testimonials</h1>
             <div className='m-5 mt-0 w-50 mx-auto'>
-                <p>“What a phenomenal experience! We contacted and hired Rob via Facetime. Rob and his crew nailed it. The pictures turned out amazing. Too good actually... the hardest part was narrowing our photographs down for our wedding book!”</p>
+                <p>“What a phenomenal experience! We contacted and hired normal package. She nailed it. The pictures turned out amazing. Too good actually... the hardest part was narrowing our photographs down for our wedding book!”</p>
                 <h6 className='text-end'>    — — DORA + MARKO OF PHILADELPHIA, PA</h6>
-                <p className='mt-5'>“Rob and his team are extraordinary in kindness, response time, creativity, professionalism, and value. The quality of their photography and cinematography are amazing! We absolutely love our premium Italian wedding photo book.”</p>
+                <p className='mt-5'>“She is extraordinary in kindness, response time, creativity, professionalism, and value. The quality of her photography and cinematography are amazing! We absolutely love our premium Italian wedding photo book.”</p>
                 <h6 className='text-end'>— — MANDY + EDDIE OF SCRANTON, PA</h6>
             </div>
         </div>
